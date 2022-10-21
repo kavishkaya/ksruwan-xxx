149c2c304f3295634f6bdbc2f3e526e1bf217953
erwjfixofdifdjcfidf <111345267+erwjfixofdifdjcfidf@users.noreply.github.com>
2022-10-21 15:48:17 +0530

    Update someonesay.js

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -1,16 +1,17 @@
 
-const Neotro = require('../events');
+const evt = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-
+const config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('ttp');
+const QUEEN = config.WORKTYPE == 'public' ? true : ''
+const DIANA = config.WORKTYPE == 'private' ? true : false
 
 
-
-    Neotro.addCommand({ pattern: 'animesay ?(.*)', fromMe: true, desc: Lang.ANIME_DESC }, (async (message, match) => {
+    evt.getCMD({ pattern: 'animesay ?(.*)', fromMe: QUEEN, desc: Lang.ANIME_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
@@ -20,7 +21,7 @@ const Lang = Language.getString('ttp');
 
     }));
 
-    Neotro.addCommand({ pattern: 'changesay ?(.*)', fromMe: true, desc: Lang.CHANGE_DESC }, (async (message, match) => {
+    evt.getCMD({ pattern: 'changesay ?(.*)', fromMe: QUEEN, desc: Lang.CHANGE_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
@@ -30,7 +31,7 @@ const Lang = Language.getString('ttp');
 
     }));
 
-    Neotro.addCommand({ pattern: 'trumpsay ?(.*)', fromMe: true, desc: Lang.TRUMP_DESC }, (async (message, match) => {
+    evt.getCMD({ pattern: 'trumpsay ?(.*)', fromMe: QUEEN, desc: Lang.TRUMP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
@@ -40,3 +41,33 @@ const Lang = Language.getString('ttp');
 
     }));
 
+
+    evt.getCMD({ pattern: 'animesay ?(.*)', fromMe: DIANA, desc: Lang.ANIME_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=kannagen&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,{ image: Buffer.from(ttinullimage.data), caption: Config.CAPTION},  { quoted: message.data } )
+
+    }));
+
+    evt.getCMD({ pattern: 'changesay ?(.*)', fromMe: DIANA, desc: Lang.CHANGE_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,{ image: Buffer.from(ttinullimage.data), caption: Config.CAPTION},  { quoted: message.data } )
+
+    }));
+
+    evt.getCMD({ pattern: 'trumpsay ?(.*)', fromMe: DIANA, desc: Lang.TRUMP_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=trumptweet&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,{ image: Buffer.from(ttinullimage.data), caption: Config.CAPTION},  { quoted: message.data } )
+
+    }));
