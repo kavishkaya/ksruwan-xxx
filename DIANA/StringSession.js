de4e89abf93e9b58a2adc6736749f4a23be83eab
kavishka <91202334+kavishkaya@users.noreply.github.com>
2023-01-24 13:43:56 +0530

    Update StringSession.js

--- DIANA/StringSession.js
+++ DIANA/StringSession.js
@@ -1,4 +1,4 @@
-/*const fs = require("fs");
+const fs = require("fs");
 var { File } = require("megajs");
 const config = require('../config');
 
@@ -25,4 +25,4 @@ fs.writeFileSync('/root/queendianamd/DIANA/diaAuth.json', data.toString())
 }
 
 module.exports = StringSession;
-*/
+
